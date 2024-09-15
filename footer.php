66f2e47a54cd30d91f053ea7bb4a225994a49cc2
elmahtoasean <toasean.csecu@gmail.com>
2024-09-15 22:19:42 +0600

    Created capCon.php, capital.php and mofified footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<div id="popup" class="popup">
+  <div class="popup-content">
+    <h2>Logout Confirmation</h2>
+    <p>Are you sure you want to logout?</p>
+    <div class="buttoncontainer">
+      <a href="logoutcon.php"><button class="confirmbtn" id="logoutBtn"><i class='bx bx-log-out'></i> Yes, Logout</button></a>
+      <button class="cancelbtn" id="cancelBtn" onclick="closePopup()">Cancel</button>
+    </div>
+  </div>
+</div>
+
+
+</body>
+<script src="home1.js"></script>
+</html>
